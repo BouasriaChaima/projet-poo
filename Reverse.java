4733daf3929712d48081de3ccb05980a768f69f5
Rihane GHAZI <147904886+rihaneghazi1@users.noreply.github.com>
2024-12-29 14:11:09 +0100

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Reverse extends Card {
+    public Reverse(String color) {
+        super(color, "Reverse", -1);
+    }
+
+    public void applyEffect(ArrayList<Player> players, int currentPlayerIndex) {
+        // Idem logic : to reverse the order of play
+        System.out.println("The direction of play has been reversed!");
+        Collections.reverse(players); // Reversing the order of players
+        // i wanna keep the same player, so no need to update currentPlayerIndex here
+    }
+}
